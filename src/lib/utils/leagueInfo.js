98ea55d6960a897d16238e55f122064b9159a793
trevguidry101 <150477980+trevguidry101@users.noreply.github.com>
2023-11-10 14:43:15 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,7 +8,6 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>Founded in 2023, the The Bois Dynasty League is a dynasty league amongst a small group of friends. Each year, the managers compete to win the Bois Cup and avoid the ever-humiliating Toilet Bowl.</p>
-  <p>Founded in 2023, the The Bois Dynasty League is a dynasty league amongst a small group of friends. Each year, the managers compete to win the Bois Cup and avoid the ever-humiliating Toilet Bowl.</p>
   <p>Through the first season it has become clear which teams are competing to make an immediate run for the trophy and which teams have sacrificed immediate glory to build for the future. Only time will tell if that decision will ultimately pay off for these teams.</p>
 
 /*   STEP 3   */
