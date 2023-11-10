1d136b1cf3ce258a1989bfb1600b2d85547a0b94
trevguidry101 <150477980+trevguidry101@users.noreply.github.com>
2023-11-10 15:11:17 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -10,7 +10,7 @@ export const homepageText = `
   <p>Founded in 2023, the The Bois Dynasty League is a dynasty league amongst a small group of friends. Each year, the managers compete to win the Bois Cup and avoid the ever-humiliating Toilet Bowl.</p>
   <p>Through the first season it has become clear which teams have set their sights on an immediate glory and which teams have decided to look towards the future for their chance at a trophy. Only time will tell how these decisions pay off.</p>
   <p>While all of the team managers in this league are experienced in fantasy football, this is everyones first dynasty league. There has been a learning curve for everyone and varying strategies managers are using to run their teams shouold lead to an exciting future.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>The ultimate prize of this league is bragging rights for a year for the champion, but the extra $200 in the winners pocket will certainly be appreciated almost as much.</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 `;
